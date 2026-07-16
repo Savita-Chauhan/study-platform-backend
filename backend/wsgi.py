0bd1237c6a4ad21dcf13b429019a0aa9c1745326
Savita Chauhan <chauhansavita711@gmail.com>
2026-07-16 15:40:59 +0530

    Auto create superuser

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,6 +5,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
 
-# Auto migrate on startup
+# Auto migrate + create superuser on startup
 from django.core.management import call_command
 call_command('migrate', '--run-syncdb')
+
+# Create superuser automatically
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin2',
+        email='chauhansavita711@gmail.com',
+        password='Admin2@1234'
+    )
